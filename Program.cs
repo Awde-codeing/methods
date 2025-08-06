279d60cc2230ce39c0dcdf79c750471abc181838
Awde <Gamer@DESKTOP-QOEP6PN>
2025-08-06 10:29:26 +0200

    done med opgave 1 2,3 og 3 a

--- Program.cs
+++ Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
+        #region opgave 1
+        public static string HelloWorld()
+        {
+
+            return "Hello World!";
+        } 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string world = HelloWorld();
+            Console.WriteLine(world);
+            Console.ReadKey();
+
         }
+        #endregion
     }
 }
